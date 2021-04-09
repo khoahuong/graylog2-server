34197db729a3ba87c131428b2cf40ac7dc1d1178
Linus Pahl <linus@graylog.com>
2021-04-09 15:53:44 +0200

    Removing min height and top padding for all `Checkbox` components.

--- graylog2-web-interface/src/views/components/aggregationwizard/elementConfigurationSections/ElementConfigurationSection.tsx
+++ graylog2-web-interface/src/views/components/aggregationwizard/elementConfigurationSections/ElementConfigurationSection.tsx
@@ -53,6 +53,11 @@ const Wrapper = styled.div(({ theme }) => css`
     margin: 0;
     hyphens: auto;
   }
+
+  .checkbox {
+    min-height: auto;
+    padding-top: 0;
+  }
 `);
 
 const Header = styled.div`
